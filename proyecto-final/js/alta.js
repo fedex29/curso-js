6063b065344d8a78afca0f415d29b5d25e9d0824
fedex29 <frodriguez@trepcom.com>
2022-06-11 13:25:03 -0300

    proyecto final

---
+++ proyecto-final/js/alta.js
@@ -0,0 +1,72 @@
+﻿//Alta libros en biblioteca
+
+class Libro {
+    constructor(titulo,categoria,paginas){
+        this.titulo = titulo;
+        this.categoria = categoria;
+        this.paginas = paginas;
+    }
+}
+
+let libros = [];
+
+if(localStorage.getItem('Libros')) {
+    libros = JSON.parse(localStorage.getItem('Libros'))
+} else {
+    localStorage.setItem('Libros', JSON.stringify(libros))
+}
+
+//Libros Dummy
+const libro1 = new Libro ("Aprende a cocinar","Cocina",250);
+const libro2 = new Libro ("Cocina for dummies","Cocina",150);
+const libro3 = new Libro ("Historia del fobal","Deportes",300);
+const libro4 = new Libro ("7 pasos para organizarse","Hogar",400);
+const libro5 = new Libro ("La Copa Libertadores","Deportes",1000);
+const libro6 = new Libro ("Carpinteria básica","Hogar",500);
+const libro7 = new Libro ("Ignoto","Suspenso",650);
+const libro8 = new Libro ("¿Quien se llevo a Juan?","Suspenso",850);
+
+let librosDummy = document.querySelector("#librosDummy");
+
+librosDummy.addEventListener('click', () => {
+    libros.push(libro1)
+    libros.push(libro2)
+    libros.push(libro3)
+    libros.push(libro4)
+    libros.push(libro5)
+    libros.push(libro6)
+    libros.push(libro7)
+    libros.push(libro8)
+    console.log(libros)
+    localStorage.setItem('Libros', JSON.stringify(libros))
+    alert('Libros dummy cargados correctamente');
+    window.location.reload();
+})
+
+let altaLibro = document.querySelector('#altaLibro')
+
+altaLibro.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let altaForm = new FormData(e.target)
+    const libro = new Libro(altaForm.get('titulo'), altaForm.get('categoria'), altaForm.get('paginas'))
+    libros.push(libro)
+    Swal.fire(
+        altaForm.get('titulo'),
+        'Se dio de alta correctamente',
+        'success'
+      )
+    localStorage.setItem('Libros', JSON.stringify(libros))
+    altaLibro.reset()
+})
+
+
+let borrarLibros = document.querySelector('#borrarLibros')
+
+borrarLibros.addEventListener('click', () => {
+    window.localStorage.clear();
+    Swal.fire(
+        'Libros eliminados',
+        '',
+        'error'
+      )
+})
---
+++ proyecto-final/js/script.js
@@ -0,0 +1,59 @@
+﻿//Simulador biblioteca
+
+class Libro {
+    constructor(titulo,categoria,paginas){
+        this.titulo = titulo;
+        this.categoria = categoria;
+        this.paginas = paginas;
+    }
+}
+
+let libros = [];
+
+if(localStorage.getItem('Libros')) {
+    libros = JSON.parse(localStorage.getItem('Libros'))
+} else {
+    localStorage.setItem('Libros', JSON.stringify(libros))
+}
+
+let inputTexto = document.getElementById("inputTexto");
+let divResultadoCategorias = document.getElementById("divResultadoCategorias");
+
+inputTexto.addEventListener('change', () => {
+    let buscador = inputTexto.value
+    console.log(buscador)
+    let librosFiltrados = JSON.parse(localStorage.getItem('Libros')).filter(libro => libro.categoria.includes(buscador))
+
+    librosFiltrados.forEach(libro => {
+        divResultadoCategorias.innerHTML += `
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+            <h5 class="card-title">${libro.titulo}</h5>
+                <p>Categoria: ${libro.categoria} </p>
+                <p>Páginas: ${libro.paginas} </p>
+            </div>
+        </div>
+        `
+    })
+})
+
+
+//Uso de API para obtener cotización del dolar en Uruguay
+//Fuente: https://github.com/gmanriqueUy/cotizaciones-brou
+
+let divCotizacionDolar = document.getElementById("divCotizacionDolar");
+
+fetch("https://cotizaciones-brou.herokuapp.com/api/currency/latest")
+.then(response => response.json())
+.then(data => {
+    //Filtro solo por los rates
+    let {rates} = data
+    //Obtengo solo USD
+    let {USD} = rates
+    //Obtengo el valor de venta
+    let {sell, buy} = USD
+    divCotizacionDolar.innerHTML += `
+    Compra: $${buy}<br>
+    Venta: $${sell}
+    `
+})
